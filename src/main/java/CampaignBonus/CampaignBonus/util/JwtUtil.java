8de0b5db405beadd3a57745856152cc58da26224
Chientd <taducchiennb01@gmail.com>
2024-10-20 14:41:01 +0700

    fix

--- src/main/java/CampaignBonus/CampaignBonus/util/JwtUtil.java
+++ src/main/java/CampaignBonus/CampaignBonus/util/JwtUtil.java
@@ -3,25 +3,24 @@ package CampaignBonus.CampaignBonus.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.stereotype.Component;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-  private final String secret = "secret_key";
-  private final long expirationTime = 86400000; // 1 day
+  private final String secret = "your-very-long-secret-key-of-at-least-32-characters";
+  private final long expirationTime = 86400000; // Token expiration time (24 hours)
 
+  private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
   public String generateToken(String username) {
-
-    Key key = Keys.hmacShaKeyFor(secret.getBytes());
     return Jwts.builder()
             .setSubject(username)
             .setIssuedAt(new Date())
             .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-            .signWith(key, SignatureAlgorithm.HS256)
+            .signWith(key, SignatureAlgorithm.HS256) // Use the Key instance here
             .compact();
   }
 
@@ -37,4 +36,3 @@ public class JwtUtil {
     return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
   }
 }
-
